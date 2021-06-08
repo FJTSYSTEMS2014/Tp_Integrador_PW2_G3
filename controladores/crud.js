5df3e9ec97f4898b62eb4dd6d4f699f9f55a2d4e
Franck_Tscherig <fjtsystemsg@gmail.com>
2021-06-08 01:03:50 -0300

    modificacion de actualziaciones

--- controladores/crud.js
+++ controladores/crud.js
@@ -1,137 +1,151 @@
-const conexion=require('../database/db');
+const conexion = require ('../database/db');
 // aca preguntamos o verificamos si exist el
 // usuario y contraseña.
 
 // para guardar el metodo es salvar lo podemos ver en crear.ejs
-exports.salvar=(req,res)=>{
-    console.log("entro a salvar usuario");
-    // capturamos los campos del usuario.
-    // para invocar a estos valores los tenemos que configurar en rutas.js.
-   const user= req.body.user;
+exports.salvar = (req, res) => {
+  console.log ('entro a salvar usuario');
+  // capturamos los campos del usuario.
+  // para invocar a estos valores los tenemos que configurar en rutas.js.
+  const user = req.body.user;
 
-   const sql = 'INSERT INTO usuarios SET ?';
-   const info = {
-     dni_usuario: user[0],
-     nombre_apellido: user[1],
-     fecha_nacimiento: user[2],
-     domicilio: user[3],
-     localidad: user[4],
-     telefono: user[5],
-     username: user[6],
-     pass: user[7],
-   };
-   conexion.query (sql, info, err => {
+  const sql = 'INSERT INTO usuarios SET ?';
+  const info = {
+    dni_usuario: user[0],
+    nombre_apellido: user[1],
+    fecha_nacimiento: user[2],
+    domicilio: user[3],
+    localidad: user[4],
+    telefono: user[5],
+    username: user[6],
+    pass: user[7],
+  };
+  conexion.query (sql, info, err => {
     if (err) {
       console.log (err);
-     
     } else {
       console.log (' usuario agregado');
-      res.redirect('/')
-
+      res.redirect ('/');
     }
   });
-console.log(info);
-
+  console.log (info);
 };
 
-
 // para guardar el metodo es salvar lo podemos ver en crear_tarea.ejs
-exports.salvar_tarea=(req,res)=>{
-  console.log("entro a salvar_tarea");
+exports.salvar_tarea = (req, res) => {
+  console.log ('entro a salvar_tarea');
   // capturamos los campos de la tarea.
   // para invocar a estos valores los tenemos que configurar en rutas.js.
- const user= req.body.user;
- const id= req.body.getID;
- //const dni_usuario= 'DNI33.333.333'
- /*
+  const user = req.body.user;
+  const id = req.body.getID;
+  //const dni_usuario= 'DNI33.333.333'
+  /*
  <%= dni_usuario  %>  */
- const sql = 'INSERT INTO tareas SET ?';
- const info = {
-  dni_usuario: user[0],
-   titulo: user[1],
-   descripcion: user[2],
-   estado: user[3],
-   created: user[4],
-   updated: user[5],
-   eliminated: user[6],
- }; 
- conexion.query (sql, info, err => {
-  if (err) {
-    console.log (err);
-   
-  } else {
-    console.log (' tarea agregada');
-    res.redirect('/tareas')
-
-  }
-});  
-console.log(user);
-
-
+  const sql = 'INSERT INTO tareas SET ?';
+  const info = {
+    dni_usuario: user[0],
+    titulo: user[1],
+    descripcion: user[2],
+    estado: user[3],
+    created: user[4],
+    updated: user[5],
+    eliminated: user[6],
+  };
+  conexion.query (sql, info, err => {
+    if (err) {
+      console.log (err);
+    } else {
+      console.log (' tarea agregada');
+      res.redirect ('/tareas');
+    }
+  });
+  console.log (user);
 };
 
 
 
 // para guardar el metodo es salvar lo podemos ver en index.ejs
 
-exports.verify=(req,res)=>{
-    console.log("entro a verify");
-    // capturamos los campos del usuario.
-    // para invocar a estos valores los tenemos que configurar en rutas.js.
-   const user= req.body.user;
-   console.log(user+ " " );
-   const username=user[0];
-   const pass=user[1];
+exports.verify = (req, res) => {
+  // capturamos los campos del usuario.
+  // para invocar a estos valores los tenemos que configurar en rutas.js.
+  const user = req.body.user;
+  console.log (user + ' ');
+  const username = user[0];
+  const pass = user[1];
 
-if(username==undefined ||username==""|| pass==undefined||pass==""){
-  console.log('DATOS indDEFINIDOS');
-  res.render('404');
-  
-} else {
-
-
-// ACa tenemos los datos de usuario y contraseña DESDE EL LOGIN
-// FUNCION VERIFY
-// documentacion:https://www.npmjs.com/package/mysql2?activeTab=readme
-const resultado= conexion.execute(
-  'SELECT `dni_usuario` FROM `usuarios` WHERE `username` = ? AND `pass` = ?',
-  [username,pass],  function(err, resultado) {
-
-    const dni_usuario= resultado[0].dni_usuario;    }    );
-  res.redirect('/listar')
+  if (username && pass) {
+    console.log ('entro a verify');
+    conexion.query (
+      'SELECT * FROM usuarios WHERE username = ?',
+      [username],
+      (error, results, fields) => {
+        console.log (results);
+        console.log (results[0].pass);
+        if (results.length == 0 || pass != results[0].pass) {
+          console.log ('usuario invalido');
+          res.render ('404');
+        } else {
+          console.log ('usuario correcto');
+          res.redirect ('/listar');
+        }
+        console.log ('esta por aca ');
+      }
+    );
+  } else {
+    console.log ('usuario invalido');
+    res.render ('404');
   }
 };
 
+// actualizar ver edit.ejs y rutas
+exports.update = (req, res) => {
+  // capturo el dni old
+  const dni_usuario_old = req.body.id;
+  // en user vendria con el dni nuevo
+  const usuario = req.body.user;
+  const dni_usuario = usuario[0];
+  const nombre_apellido_ = usuario[1];
+  const fecha_nacimiento0 = usuario[2]; 
+  const today = new Date (fecha_nacimiento0);
+  //console.log (today.toISOString ()); // info que mando EDU por chat 2021-06-07T03:00:00.000Z
+  const fecha_nacimiento3=today.toISOString ();
+const fecha_nacimiento = fecha_nacimiento3.substring(0, 10);
+const domicilio = usuario[3]; 
+const localidad = usuario[4]; 
+const telefono = usuario[5]; 
+const username=usuario[6];
+const pass=usuario[7];
+// fuunciona ok  
+
+/*     'UPDATE `usuarios` SET nombre_apellido= ? ,fecha_nacimiento=?, domicilio= ? WHERE `dni_usuario` = ? ',
+    [nombre_apellido_,fecha_nacimiento,domicilio, dni_usuario], */
+  conexion.query (
+    'UPDATE `usuarios` SET dni_usuario =?, nombre_apellido= ? ,fecha_nacimiento=?, domicilio= ? WHERE `dni_usuario` = ? ',
+    [dni_usuario,nombre_apellido_,fecha_nacimiento,domicilio, dni_usuario_old],
+    (error, results) => {
+      if (error) {
+        console.log (error);
+      } else {
+        res.redirect ('/listar');
+      }
+    }
+  );
+
+  // ir al enrutador para decirle que actualice con /update
+};
 
 // actualizar ver edit.ejs y rutas
-exports.update=(req,res)=>{
+exports.update_tareas = (req, res) => {
   // capturo el dni old
-const dni_usuario=req.body.id;
-// en user vendria con el dni nuevo
-const usuario=req.body.user;
+  const id = req.body.id;
+  // en user vendria con el dni nuevo
+  const usuario = req.body.user;
 
+  console.log (usuario);
+  res.redirect('/listar');
 
-console.log('entro  a actualizar /update')
-// aca tenemos que ver todos los campos atualizados
-console.log(`el dni original es: ${dni_usuario}`);
-console.log(usuario);
-conexion.query('UPDATE usuarios SET ? WHERE dni_usuario = ?',[{usuario:usuario},dni_usuario],(error, results)=>{
-
-  if (error) {
-    console.log (error);
-   
-  } else {
-  
-    res.redirect('/')
-
-  }
-
-})
-// ir al enrutador para decirle que actualice con /update
-}
-
-
-
+};
 
 // verificar token
 
--- rutas.js
+++ rutas.js
@@ -1,180 +1,181 @@
 const express = require ('express');
 const rutas = express.Router ();
-const conexion=require('./database/db');
+const conexion = require ('./database/db');
 // nuevo para utilizar JWT
 const jwt = require ('jsonwebtoken');
 
 rutas.get ('/', (req, res) => {
- //iniciar vistas en la carpeta views
-// pedir datos al server
+  //iniciar vistas en la carpeta views
+  // pedir datos al server
 
-    // aca hacemos una consulta por mysql
- // por convencion se utiliza error y results
- conexion.query('SELECT * FROM usuarios',(error, resultado)=>{
-
-    if(error){
-        console.log(error);
-    } else{
-        // la variable resultado se la paso a login con una variable resultado.
+  // aca hacemos una consulta por mysql
+  // por convencion se utiliza error y results
+  conexion.query ('SELECT * FROM usuarios', (error, resultado) => {
+    if (error) {
+      console.log (error);
+    } else {
+      // la variable resultado se la paso a login con una variable resultado.
       //  res.render('login',{resultado:resultado})
-      res.render('index2',{resultado:resultado})
+      res.render ('index2', {resultado: resultado});
     }
-
- })
-
+  });
 });
 
-rutas.get('/listar', (req, res, next) => {
-  
-    conexion.query('SELECT * FROM usuarios',(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.render('usuarios.ejs', {results:results});            
-        }   
-    }) });
-    rutas.get('/edit', (req, res, next) => {
-  
-                           
-                res.render('editar.ejs');            
-            }   
-        ) 
+rutas.get ('/listar', (req, res, next) => {
+  conexion.query ('SELECT * FROM usuarios', (error, results) => {
+    if (error) {
+      console.log (error);
+    } else {
+      res.render ('usuarios.ejs', {results: results});
+    }
+  });
+});
+rutas.get ('/edit', (req, res, next) => {
+  res.render ('editar.ejs');
+});
 
 // voy al formulario de registro
-    rutas.get ('/registro', (req, res) => {
-                
-                conexion.query('SELECT * FROM usuarios',(error, results)=>{
-                    if(error){
-                        throw error;
-                    } else {                       
-                        res.render('crear.ejs');              
-                    }   
-                }) });
-                // voy al formulario de crear tareas  x dni
-    rutas.get ('/add_tareas/:dni_usuario', (req, res) => {
-        const dni_usuario=req.params.dni_usuario;   
-        // aca ya tenemos el dni del que va a crear la tarea
-        // debemos de alguna manera ingresarlo al dni_usuario y que no cambie 
-        console.log(dni_usuario)  ;
-                
-        conexion.query('SELECT * FROM tareas',(error, results)=>{
-            if(error){
-                throw error;
-            } else {                       
-                res.render('crear_tareas.ejs',{dni_usuario:dni_usuario});              
-            }   
-        }) });
+rutas.get ('/registro', (req, res) => {
+  conexion.query ('SELECT * FROM usuarios', (error, results) => {
+    if (error) {
+      console.log (error);
+    } else {
+      res.render ('crear.ejs');
+    }
+  });
+});
+// voy al formulario de crear tareas  x dni
+rutas.get ('/add_tareas/:dni_usuario', (req, res) => {
+  const dni_usuario = req.params.dni_usuario;
+  // aca ya tenemos el dni del que va a crear la tarea
+  // debemos de alguna manera ingresarlo al dni_usuario y que no cambie
 
+  conexion.query ('SELECT * FROM tareas', (error, results) => {
+    if (error) {
+      console.log (error);
+    } else {
+      res.render ('crear_tareas.ejs', {dni_usuario: dni_usuario});
+    }
+  });
+});
 
+rutas.get ('/tareas', (req, res) => {
+  conexion.query ('SELECT * FROM tareas', (error, results) => {
+    if (error) {
+      console.log (error);
+    } else {
+      res.render ('tareasDNI.ejs', {results: results});
+    }
+  });
+});
 
-    
-
-    rutas.get ('/tareas', (req, res) => {
-                
-                    conexion.query('SELECT * FROM tareas',(error, results)=>{
-                        if(error){
-                            throw error;
-                        } else {                       
-                            res.render('tareasDNI.ejs', {results:results});              
-                        }   
-                    }) });
-
-
-const crud=require('./controladores/crud');
-  // agregar usuarios 
-rutas.post('/salvar',crud.salvar);
+const crud = require ('./controladores/crud');
+// agregar usuarios
+rutas.post ('/salvar', crud.salvar);
 // AGREGAR tarea
-rutas.post('/salvar_tarea',crud.salvar_tarea);
-  // verify usuarios 
- rutas.post('/verify',crud.verify);
- //actualizar usuarios
- rutas.post('/update',crud.update);
-
- 
+rutas.post ('/salvar_tarea', crud.salvar_tarea);
+// verify usuarios
+rutas.post ('/verify', crud.verify);
+//actualizar usuarios
+rutas.post ('/update', crud.update);
+//actualizar tarea
+rutas.post ('/update_tareas', crud.update_tareas);
 
 // editar el archivos usuarios por dni (primary key)
-    rutas.get ('/edit/:dni_usuario', (req, res) => {
-        console.log('entro en /edit/:dni_usuario')
-        const dni_usuario=req.params.dni_usuario;   
-        console.log(dni_usuario)  ;
-        conexion.query('SELECT * FROM usuarios WHERE dni_usuario=?' ,[dni_usuario],(error, results)=>{
-            if(error){
-                throw error;
-            } else {                       
-                res.render('edit', {user:results[0]}); 
-                console.log(results);           
-            }   
-        }) });
+rutas.get ('/edit/:dni_usuario', (req, res) => {
 
+  const dni_usuario = req.params.dni_usuario;
+
+  conexion.query (
+    'SELECT * FROM usuarios WHERE dni_usuario=?',
+    [dni_usuario],
+    (error, results) => {
+      if (error) {
+        console.log (error);
+      } else {
+        res.render ('edit', {user: results[0]});
+      }
+    }
+  );
+});
 
 // editar el archivos tareas por dni (F key)
-rutas.get ('/edit_tareas/:dni_usuario', (req, res) => {
-    console.log('entro en /edit/:dni_usuario')
-    const dni_usuario=req.params.dni_usuario;   
-    console.log(dni_usuario)  ;
-    conexion.query('SELECT * FROM tareas WHERE dni_usuario=?' ,[dni_usuario],(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.render('edit_tareas', {user:results[0]}); 
-            console.log(results);           
-        }   
-    }) });
+rutas.get ('/edit_tareas/:id', (req, res) => {
+  console.log ('entro en /edit_tareas/:dni_usuario');
+  const id = req.params.id;
 
+  conexion.query (
+    'SELECT * FROM tareas WHERE id=?',
+    [id],
+    (error, results) => {
+      if (error) {
+        console.log (error);
+      } else {
+       
+        res.render ('edit_tareas', {user: results[0]});
+      }
+    }
+  );
+});
 
-
-
-        // elimninar usuarios ver: el delete de usuarios.ejs
-rutas.get('/delete/:dni_usuario', (req, res) => {
-    console.log('entro en /delete/:dni_usuario')
-    const dni_usuario=req.params.dni_usuario;   
-    console.log(`el DNI a borrar es: ${dni_usuario}`)  ;
-    conexion.query('DELETE FROM usuarios WHERE dni_usuario= ?',[dni_usuario],(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.redirect('/listar'); 
-            console.log('usuario eliminado');           
-        }   
-    })
-
-})
-
+// elimninar usuarios ver: el delete de usuarios.ejs
+rutas.get ('/delete/:dni_usuario', (req, res) => {
+  console.log ('entro en /delete/:dni_usuario');
+  const dni_usuario = req.params.dni_usuario;
+  console.log (`el DNI a borrar es: ${dni_usuario}`);
+  conexion.query (
+    'DELETE FROM usuarios WHERE dni_usuario= ?',
+    [dni_usuario],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.redirect ('/listar');
+      }
+    }
+  );
+});
 
 // crear tareas por DNI --> seguir en crud
 rutas.get ('/add/:dni_usuario', (req, res) => {
-    console.log('entro a /add/:dni_usuario');
+  conexion.query ('SELECT * FROM tareas', (error, results) => {
+    if (error) {
+        console.log(error);
+    } else {
+      res.render ('crear_tareas.ejs');
+    }
+  });
+});
 
-    conexion.query('SELECT * FROM tareas',(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.render('crear_tareas.ejs');              
-        }   
-    }) });
-
+// ver todas las tareas  por DNI --> seguir en crud
+rutas.get ('/view_tareas/:dni_usuario', (req, res) => {
+  const dni_usuario = req.params.dni_usuario;
+  conexion.query (
+    'SELECT * FROM tareas WHERE dni_usuario = ? ',
+    [dni_usuario],
+    (error, results) => {
+      if (error) {
+        throw error;
+      } else {
+        res.render ('tareasDNI.ejs', {results: results});
+      }
+    }
+  );
+});
 
 // elimninar tareas x usuarios rs
-         rutas.get('/delete_tareas/:dni_usuario', (req, res) => {
-            console.log('entro en /delete_tareas/:dni_usuario')
-            const dni_usuario=req.params.dni_usuario;   
-            console.log(`la tareaa borrar --> corresponde al DNI: ${dni_usuario}`)  ;
-            conexion.query('DELETE FROM tareas WHERE dni_usuario= ?',[dni_usuario],(error, results)=>{
-                if(error){
-                    throw error;
-                } else {                       
-                    res.redirect('/tareas'); 
-                    console.log('tarea eliminada');           
-                }   
-            })
-        
-        })
+rutas.get ('/delete_tareas/:id', (req, res) => {
+  const id = req.params.id;
 
+  conexion.query ('DELETE FROM tareas WHERE id= ?', [id], (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.redirect ('/tareas');
+    }
+  });
+});
 
-        // editar el archivos usuarios por dni (primary key)
-  
+// editar el archivos usuarios por dni (primary key)
 
-
-
-  
 module.exports = rutas;
